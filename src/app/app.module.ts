8a81607cd98b3bde78a6bf7b111d04ab35aee3ac
unknown <juancarlos.gruposinfinito@gmai.com>
2019-03-01 02:02:02 -0600

    Listo para subir

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HttpModule } from "@angular/http";
+
+//Rutas
+import { app_routing } from "./app.routes";
+//servicios
+import { InformacionService } from "./services/informacion.service";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProductoComponent } from './components/producto/producto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent,
+    AboutComponent,
+    ProductoComponent
+  ],
+  imports: [
+    BrowserModule,
+     app_routing,
+     HttpModule,
+    AppRoutingModule
+  ],
+  providers: [
+    InformacionService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
